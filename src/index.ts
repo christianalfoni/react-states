cb8b6104a827293dbe426075b82f237f2435d33f
Christian Alfoni <christianalfoni@gmail.com>
2021-04-11 11:59:25 +0200

    add matches

--- src/index.ts
+++ src/index.ts
@@ -34,41 +34,56 @@ export type PickState<C extends { state: string }, SS extends C['state']> = C ex
 
 export type PickAction<E extends { type: string }, T extends E['type']> = E extends { type: T } ? E : never;
 
-export const transitions = <C extends TContext, A extends TAction>(
+export function transitions<C extends TContext, A extends TAction>(
   transitions: {
     [State in C['state']]: {
       [Type in A['type']]?: (action: A & { type: Type }, context: C & { state: State }) => C;
     };
   },
-): ((context: C, action: A) => C) => (context, action) => {
-  let newContext = context;
-  // @ts-ignore
-  if (transitions[context.state] && transitions[context.state][action.type]) {
+): (context: C, action: A) => C {
+  return (context, action) => {
+    let newContext = context;
     // @ts-ignore
-    newContext = transitions[context.state][action.type](action, context);
-  } else {
+    if (transitions[context.state] && transitions[context.state][action.type]) {
+      // @ts-ignore
+      newContext = transitions[context.state][action.type](action, context);
+    } else {
+      // @ts-ignore
+      action[DEBUG_IS_ACTION_IGNORED] = true;
+    }
+
     // @ts-ignore
-    action[DEBUG_IS_ACTION_IGNORED] = true;
-  }
+    newContext[DEBUG_TRANSITIONS] = transitions;
 
-  // @ts-ignore
-  newContext[DEBUG_TRANSITIONS] = transitions;
+    return newContext;
+  };
+}
 
-  return newContext;
-};
-
-export const exec = <C extends TContext>(context: C, effects: TEffects<C>) => {
+export function exec<C extends TContext>(context: C, effects: TEffects<C>) {
   // @ts-ignore
   if (effects[context.state]) {
     // @ts-ignore
     return context[DEBUG_EXEC] ? context[DEBUG_EXEC](effects[context.state]) : effects[context.state](context);
   }
-};
+}
 
-export const match = <C extends TContext, T extends TMatch<C>>(
+export function match<C extends TContext, T extends TMatch<C>>(
   context: C,
   matches: T,
 ): {
   [K in keyof T]: T[K] extends () => infer R ? R : never;
+}[keyof T] {
   // @ts-ignore
-}[keyof T] => (matches[context.state] ? matches[context.state](context) : null);
+  return matches[context.state] ? matches[context.state](context) : null;
+}
+
+export function matches<C extends TContext, A extends TAction, S extends C['state']>(
+  reducer: StatesReducer<C, A>,
+  state: S,
+): reducer is StatesReducer<C & { state: S }, A> {
+  if (reducer[0].state === state) {
+    return true;
+  }
+
+  return false;
+}
