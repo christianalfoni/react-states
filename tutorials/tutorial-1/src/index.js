c63b686daba515771b887102ad283d8781a9500c
Christian Alfoni <christianalfoni@gmail.com>
2021-08-25 11:33:00 +0200

    Update index.js

--- tutorials/tutorial-1/src/index.js
+++ tutorials/tutorial-1/src/index.js
@@ -1,7 +1,7 @@
 import './styles.css';
 
 document.getElementById('app').innerHTML = `
-<h1>Hello Vanilla!</h1>
+<h1>Hello Project!</h1>
 <div>
   We use the same configuration as Parcel to bundle this sandbox, you can find more
   info about Parcel 
